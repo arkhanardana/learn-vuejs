dd5b9b8699aed8ea82a1908772af16224ea1c9b4
arkhanardana <hanzmantap@gmail.com>
2025-06-22 22:00:33 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Layout from "./components/Layout.vue";
+import UserRegister from "./components/User/UserRegister.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      component: Layout,
+      children: [
+        {
+          path: "/register",
+          component: UserRegister,
+        },
+      ],
+    },
+    {
+      path: "/dashboard",
+    },
+  ],
+});
+
+createApp(App).use(router).mount("#app");
