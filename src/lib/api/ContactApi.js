e22f4de91f6e4a0ebc97f168efb7bf72c97dc072
arkhanardana <hanzmantap@gmail.com>
2025-06-24 22:16:33 +0700

    feat: add helper for get contact by id

--- src/lib/api/ContactApi.js
+++ src/lib/api/ContactApi.js
@@ -11,6 +11,17 @@ export const getContacts = async (token) => {
   });
 };
 
+export const getContact = async (token, id) => {
+  return await fetch(`${API_URL}/contacts/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
+};
+
 export const createContact = async (token, { first_name, last_name, email, phone }) => {
   return await fetch(`${API_URL}/contacts`, {
     method: "POST",
