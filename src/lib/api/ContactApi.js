1249abf70b4bd94544e3b4a498ff8fbcb1a5fc31
arkhanardana <hanzmantap@gmail.com>
2025-06-24 13:21:25 +0700

    feat: create helper for fetch contact API

---
+++ src/lib/api/ContactApi.js
@@ -0,0 +1,29 @@
+import { API_URL } from "./UserApi";
+
+export const getContacts = async (token) => {
+  return await fetch(`${API_URL}/contacts`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
+};
+
+export const createContact = async (token, { first_name, last_name, email, phone }) => {
+  return await fetch(`${API_URL}/contacts`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      email,
+      phone,
+    }),
+  });
+};
