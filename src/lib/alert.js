36f6a7497b6afc292c77a8a79c769c9752301d1f
arkhanardana <hanzmantap@gmail.com>
2025-06-26 19:28:34 +0700

    feat: add confirmation alert functionality using SweetAlert2

--- src/lib/alert.js
+++ src/lib/alert.js
@@ -15,3 +15,18 @@ export const alertError = async (message) => {
     text: message,
   });
 };
+
+export const alerConfirm = async (message) => {
+  const alert = await Swal.fire({
+    icon: "warning",
+    title: "Confirm",
+    text: message,
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+  });
+
+  return alert.isConfirmed;
+};
