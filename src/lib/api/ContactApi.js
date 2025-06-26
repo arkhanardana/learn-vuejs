480d9bb4f17f2e704160a67f81d1f14d60f20ead
arkhanardana <hanzmantap@gmail.com>
2025-06-26 19:28:45 +0700

    fix: remove redundant Content-Type header from getContact function

--- src/lib/api/ContactApi.js
+++ src/lib/api/ContactApi.js
@@ -21,7 +21,6 @@ export const getContact = async (token, id) => {
   return await fetch(`${API_URL}/contacts/${id}`, {
     method: "GET",
     headers: {
-      "Content-type": "application/json",
       Accept: "application/json",
       Authorization: token,
     },
@@ -31,6 +30,33 @@ export const getContact = async (token, id) => {
 export const createContact = async (token, { first_name, last_name, email, phone }) => {
   return await fetch(`${API_URL}/contacts`, {
     method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      email,
+      phone,
+    }),
+  });
+};
+
+export const deleteContact = async (token, id) => {
+  return await fetch(`${API_URL}/contacts/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
+};
+
+export const contactEdit = async (token, id, { first_name, last_name, email, phone }) => {
+  return await fetch(`${API_URL}/contacts/${id}`, {
+    method: "PUT",
     headers: {
       "Content-type": "application/json",
       Accept: "application/json",
