6384de5ebe515becb63fea45beba6eb3215cb0cb
arkhanardana <hanzmantap@gmail.com>
2025-06-28 22:11:23 +0700

    feat: add RootLayout component for handling user authentication and routing

---
+++ src/components/RootLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useLocalStorage } from "@vueuse/core";
+import { useRouter } from "vue-router";
+
+const token = useLocalStorage("token", "");
+const router = useRouter();
+
+if (!token.value || token.value === "") {
+  router.push({
+    path: "/register",
+  });
+} else {
+  router.push({
+    path: "/dashboard",
+  });
+}
+</script>
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
