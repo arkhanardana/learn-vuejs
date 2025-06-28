677f3ffe5ec0eae7505b3013732283c37189db7a
arkhanardana <hanzmantap@gmail.com>
2025-06-28 22:09:18 +0700

    feat: add AddressApi module for managing addresses linked to contacts

---
+++ src/lib/api/AddressApi.js
@@ -0,0 +1,79 @@
+import { API_URL } from "./UserApi";
+
+export const addressCreate = async (
+  token,
+  contactId,
+  { street, city, province, country, postal_code }
+) => {
+  return await fetch(`${API_URL}/contacts/${contactId}/addresses`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      street,
+      city,
+      province,
+      country,
+      postal_code,
+    }),
+  });
+};
+
+export const getAddresses = async (token, contactId) => {
+  return await fetch(`${API_URL}/contacts/${contactId}/addresses`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
+};
+
+export const updateAddress = async (
+  token,
+  contactId,
+  addressId,
+  { street, city, province, country, postal_code }
+) => {
+  return await fetch(`${API_URL}/contacts/${contactId}/addresses/${addressId}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      city,
+      street,
+      province,
+      country,
+      postal_code,
+    }),
+  });
+};
+
+export const getAddress = async (token, contactId, addressId) => {
+  return await fetch(`${API_URL}/contacts/${contactId}/addresses/${addressId}`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
+};
+
+export const deleteAddress = async (token, contactId, addressId) => {
+  return await fetch(`${API_URL}/contacts/${contactId}/addresses/${addressId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+      Authorization: token,
+    },
+  });
+};
